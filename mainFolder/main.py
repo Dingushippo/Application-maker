504c4245cc599a4ed953d93e31eea78d7280775c
Dingushippo <danbjo23@gmail.com>
2020-02-06 23:20:28 +0100

    added new version

--- mainFolder/main.py
+++ mainFolder/main.py
@@ -5,50 +5,53 @@ import time
 from datetime import date
 from mailmerge import MailMerge
 from bs4 import BeautifulSoup
+from selenium import webdriver
+
+def makeApplications():
+    template = "mainFolder/application_template.docx"
+    search_url = "https://no.jooble.org/jobb-ekstrahjelper%2Fbutikkselger-deltid/Stavanger?p="  # plus pagenumber
+    target_string = "ckey=ekstrahjelper%2fbutikkselger+deltid"
+    numOfLinks = 0
+    numOfPagesSearched = int(input("Number of pages you want searched (max 41 atm): "))
+    nameStr = input('Full name: ')
+    numStr = input('Phone number: ')
+    emailStr = input('E-mail: ')
+    linkRecurrence = {}
+    start_time = time.time()
+    for i in range(numOfPagesSearched):
+        links = []
+        html_page = urllib.request.urlopen(search_url + str(i))
+        soup_main = BeautifulSoup(html_page, features="lxml")
+        for link in soup_main.findAll('a', attrs={'href': re.compile("^https://")}):
+            if target_string in link.get('href'):
+                numOfLinks += 1
+                links.append(link.get('href'))
+
+        for link in links:
+            valueList = []
+            html_page = urllib.request.urlopen(link)
+            soup_page = BeautifulSoup(html_page, features="lxml")
+            values = soup_page.findAll(attrs={'class' : 'value-column'})
+            for value in values:
+                valueList.append(value.text.strip())
+            company = valueList[0]
+            if company == "Stavanger" or company == "Sandnes" or "/" in company or "|" in company:
+                continue
+            if company not in linkRecurrence:
+                linkRecurrence[company] = 0
+                name = company
+            else:
+                linkRecurrence[company] = linkRecurrence.get(company) + 1
+                name = f"{company}_{linkRecurrence.get(company)}"
 
 
-template = "mainFolder/application_template.docx"
-search_url = "https://no.jooble.org/jobb-ekstrahjelper%2Fbutikkselger-deltid/Stavanger?p="  # plus pagenumber
-target_string = "ckey=ekstrahjelper%2fbutikkselger+deltid"
-numOfLinks = 0
-numOfPagesSearched = int(input("Number of pages you want searched (max 41 atm): "))
-nameStr = input('Full name: ')
-numStr = input('Phone number: ')
-emailStr = input('E-mail: ')
-linkRecurrence = {}
-start_time = time.time()
-for i in range(numOfPagesSearched):
-    links = []
-    html_page = urllib.request.urlopen(search_url + str(i))
-    soup_main = BeautifulSoup(html_page, features="lxml")
-    for link in soup_main.findAll('a', attrs={'href': re.compile("^https://")}):
-        if target_string in link.get('href'):
-            numOfLinks += 1
-            links.append(link.get('href'))
+            print(f"Handling: {company}")
+            location = valueList[1]
+            document = MailMerge(template)
+            document.merge(date='{:%d-%b-%Y}'.format(date.today()), company=company, link=link, name=nameStr, email=emailStr, number=numStr)
+            document.write(f"mainFolder/applications/{name}.docx")
 
-    for link in links:
-        valueList = []
-        html_page = urllib.request.urlopen(link)
-        soup_page = BeautifulSoup(html_page, features="lxml")
-        values = soup_page.findAll(attrs={'class' : 'value-column'})
-        for value in values:
-            valueList.append(value.text.strip())
-        company = valueList[0]
-        if company == "Stavanger" or company == "Sandnes" or "/" in company or "|" in company:
-            continue
-        if company not in linkRecurrence:
-            linkRecurrence[company] = 0
-            name = company
-        else:
-            linkRecurrence[company] = linkRecurrence.get(company) + 1
-            name = f"{company}_{linkRecurrence.get(company)}"
+    print(f"Number of links handled: {numOfLinks}, time elapsed: {int(time.time()-start_time)}s")
+    print(f"Efficiency: {numOfLinks/int(time.time()-start_time)} applications per second")
 
 
-        print(f"Handling: {company}")
-        location = valueList[1]
-        document = MailMerge(template)
-        document.merge(date='{:%d-%b-%Y}'.format(date.today()), company=company, link=link, name=nameStr, email=emailStr, number=numStr)
-        document.write(f"mainFolder/applications/{name}.docx")
-
-print(f"Number of links handled: {numOfLinks}, time elapsed: {int(time.time()-start_time)}s")
-print(f"Efficiency: {numOfLinks/int(time.time()-start_time)} applications per second")
--- mainFolder/newMain.py
+++ mainFolder/newMain.py
@@ -1,5 +1,8 @@
 from selenium import webdriver as wd
+from selenium.webdriver.support.ui import Select
 from datetime import date
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common import keys
 # from mailmerge import MailMerge as mm
 from bs4 import BeautifulSoup as bs
 import urllib.request as urlR
@@ -7,38 +10,70 @@ import urllib.parse as urlP
 import time
 import re
 import math
+import secret
+import os
 
-def initialize():
-    driver = wd.Chrome()
+window = 0
+DELAY = 1
+handledPages = []
+driver = wd.Chrome()
+actions = ActionChains(driver)
 
-def primes():
-    k = 0
-    run = True
-    primeList = [2,3]
-    startTime = time.time()
-    max = int(input("Max number: "))
-    while run < max:
-        k += 1
-        a = 6*k-1
-        b = 6*k+1
-        addA =True
-        addB =True
-        for num in primeList:
-            if a%num == 0:
-                addA=False
-                break
-            if b%num == 0:
-                addB=False
-                break
-        if a > max or b > max:
+
+def init():
+    url_string = 'https://www.finn.no/job/fulltime/search.html?extent=3942&location=1.20001.20012&location=2.20001.20012.20196'
+    driver.get(url_string)
+
+def find_easy_applications():
+    divs = driver.find_elements_by_class_name('status.status--success.u-mb8')
+    link_elements = []
+    for entry in divs:
+        parent = entry.find_element_by_xpath('..').find_element_by_xpath('..')
+        parentLink = parent.find_element_by_class_name('ads__unit__link')
+        href = parentLink.get_attribute('href')
+        if href not in handledPages:
+            link_elements.append(parent)
+            print(href)
+    return link_elements
+
+def enter_easy_application(entry):
+    entry.click()
+    time.sleep(DELAY)
+    handledPages.append(driver.current_url)
+    driver.find_element_by_id('job-apply-button').click()
+    time.sleep(DELAY)
+    global window
+    window += 1
+    driver.switch_to_window(driver.window_handles[window])
+    try:
+        time.sleep(DELAY)
+        driver.find_element_by_id('username').send_keys(secret.email)
+        driver.find_element_by_id('password').send_keys(secret.password)
+        driver.find_element_by_id('ActionButton_0').click()
+        time.sleep(DELAY)
+    except:
+        pass
+    driver.find_element_by_name('attachment.file').send_keys(os.getcwd()+"/cv.docx") # File upload, use for application
+    Select(driver.find_element_by_name('education')).select_by_value('37')
+    #driver.find_element_by_name('currentTitle').clear():
+    driver.find_element_by_name('currentTitle').send_keys('Student')
+    input('Press enter to continue')
+    #######################################################################################
+    #try:
+    #    driver.find_element_by_class_name('button.primary').click() # Uncomment when ready
+    #except:
+    #    pass
+    #######################################################################################
+    time.sleep(DELAY)
+    driver.switch_to_window(driver.window_handles[0])
+    driver.back()
+    time.sleep(DELAY)
+
+def main():
+    init()
+    while True:
+        entries = find_easy_applications()
+        if len(entries) <= 0:
             break
-        if addA:
-            primeList.append(a)
-        if addB:
-            primeList.append(b)
-    print("Time: " + str(time.time()-startTime))    
-    return primeList    
-    
-    
-        
-        
+        enter_easy_application(entries[0])
+    print('Finished')
